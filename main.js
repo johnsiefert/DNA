d9da0418dbf59642511e9fa35f2a58d1aad155a9
John <johnsief12@gmail.com>
2023-12-16 15:27:22 -0500

    added factory function

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
-  return dnaBases[Math.floor(Math.random() * 4)];
-};
+  const dnaBases = ['A', 'T', 'C', 'G']
+  return dnaBases[Math.floor(Math.random() * 4)]
+}
 
 // Returns a random single stand of DNA containing 15 bases
 const mockUpStrand = () => {
-  const newStrand = [];
+  const newStrand = []
   for (let i = 0; i < 15; i++) {
-    newStrand.push(returnRandBase());
+    newStrand.push(returnRandBase())
   }
-  return newStrand;
-};
+  return newStrand
+}
 
+function paequorFactory(num, arr) {
+  return {
+    specimenNum: num,
+    dna: arr,
+    mutate() {
+      let generatedBase = returnRandBase();
+      let currentBase = dna
+      if(currentBase === generatedBase){
+        return 'T'
+      }
+    }
+  }
+}
+
+console.log(paequorFactory.mutate)
 
 
 

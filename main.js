bc3bab7a03f50a9c754372fd489b1669027135e0
John <johnsief12@gmail.com>
2023-12-19 15:45:38 -0500

    have mutate method working where changes a random index of array

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const returnRandBase = () => {
   return dnaBases[Math.floor(Math.random() * 4)]
 }
 
+
 // Returns a random single stand of DNA containing 15 bases
 const mockUpStrand = () => {
   const newStrand = []
@@ -13,21 +14,41 @@ const mockUpStrand = () => {
   return newStrand
 }
 
+
 function paequorFactory(num, arr) {
   return {
     specimenNum: num,
     dna: arr,
     mutate() {
-      let generatedBase = returnRandBase();
-      let currentBase = dna
-      if(currentBase === generatedBase){
-        return 'T'
-      }
-    }
+          let changeBase = returnRandBase()
+          let randNum = Math.floor(Math.random() * 16)
+                while(this.dna[randNum] === changeBase) {
+                 changeBase = returnRandBase()
+           }
+         this.dna[randNum] = changeBase
+
+        return this.dna
+       },
+       compareDNA(arr){
+        for(let i = 0; i > this.dna.length; i++){
+          for(let j = 0; j > arr.length; j++){
+                if(this.dna[i] === arr[j]){
+                  let sum = (this.dna[i] / arr[j]) * 100
+                  console.log(sum)
+                `specimen#${this.specimenNum} and specimen#${this.specimenNum} have ${sum.toFixed()} DNA in common}`
+              }
+          }
+        }
+       },
+
   }
 }
 
-console.log(paequorFactory.mutate)
+ const dnaStrand = paequorFactory(1,['C', 'G', 'A', 'A', 'A', 'C', 'A', 'C', 'C', 'C', 'G', 'A', 'T', 'G', 'G' ])
+ const dnaStrand2 = paequorFactory(2,['C', 'A', 'A', 'C', 'A', 'A', 'G', 'C', 'C', 'C', 'G', 'A', 'T', 'G', 'G' ])
+console.log(dnaStrand.compareDNA(['C', 'G', 'A', 'A', 'A', 'C', 'A', 'C', 'C', 'C', 'G', 'A', 'T', 'G', 'G' ]))
+console.log(dnaStrand2.compareDNA(['C', 'A', 'A', 'C', 'A', 'A', 'G', 'C', 'C', 'C', 'G', 'A', 'T', 'G', 'G' ]))
+
 
 
 
